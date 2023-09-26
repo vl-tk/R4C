8bd1be2247eb946d698b93df63877243fa58b1f9
vl-tk <vlad.work.39@ya.ru>
2023-09-26 10:29:47 +0200

    creating order

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order, WaitingListItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["customer", "robot_serial"]
+
+
+@admin.register(WaitingListItem)
+class WaitingListItemAdmin(admin.ModelAdmin):
+    list_display = ["customer", "robot_serial"]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import http
 
-# Create your views here.
+from customers.models import Customer
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order, WaitingListItem
+from robots.models import Robot
+
+
+def _create_customer(email):
+    return Customer.objects.create(email=email)
+
+
+@csrf_exempt
+def create_order(request):
+    customer = _create_customer(request.POST["email"])
+
+    try:
+        robot = Robot.objects.get(serial=request.POST["robot_serial"])
+    except Robot.DoesNotExist:
+        WaitingListItem.objects.create(
+            customer=customer, robot_serial=request.POST["robot_serial"]
+        )
+
+        return JsonResponse(
+            {"message": "We've added you to the waiting list for this robot"},
+            status=http.HTTPStatus.CREATED,
+        )
+    else:
+        order = Order.objects.create(robot_serial=robot.serial, customer=customer)
+
+    return JsonResponse(
+        {"message": f"Order {order.pk} for {order.robot_serial} created succesfully"},
+        status=http.HTTPStatus.CREATED,
+    )
